5b3de13c5cc54674dca7edf4d61809448fe41f14
Prasit Supancho <p.supancho@gmail.com>
2020-03-22 20:41:33 +0700

    Updates

---
+++ precache-manifest.781534581b3273d0e25d431451913df4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e118bb7507cbee906b0f63de22d0bbee",
+    "url": "/duck-follow/index.html"
+  },
+  {
+    "revision": "8b5f4503a1ef30c4ad28",
+    "url": "/duck-follow/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "48048983532c0679111c",
+    "url": "/duck-follow/static/js/2.f794726b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/duck-follow/static/js/2.f794726b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b5f4503a1ef30c4ad28",
+    "url": "/duck-follow/static/js/main.29ca100f.chunk.js"
+  },
+  {
+    "revision": "8d5dffd45c4c32a49c32",
+    "url": "/duck-follow/static/js/runtime-main.10a00341.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/duck-follow/static/media/logo.ee7cd8ed.svg"
+  }
+]);
