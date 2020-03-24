cd333ecd975f46b18e78406c2d4b715c07d21c71
Prasit Supancho <p.supancho@gmail.com>
2020-03-25 00:20:06 +0700

    add text policy

--- src/components/Policy.js
+++ src/components/Policy.js
@@ -8,6 +8,8 @@ export default class Policy extends React.Component {
             <div className="App">
                 <div className="App-content">
                     <h1>Policy <Badge color="secondary">privacy</Badge></h1>
+                </div>
+                <div className="App-content">
                     <h3>เมื่อคุณใช้บริการของเรา คุณไว้วางใจให้เรารักษาข้อมูลของคุณ <br/> เราเข้าใจว่านี่คือความรับผิดชอบที่ยิ่งใหญ่<br/>และพยายามอย่างยิ่งที่จะปกป้องข้อมูลของคุณ <br/> รวมถึงมอบอำนาจในการควบคุมข้อมูลให้แก่คุณ</h3>
                 </div>
             </div>
