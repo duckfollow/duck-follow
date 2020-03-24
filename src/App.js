9a9518d31a34410a60e89aa01c45e0dc90044395
Prasit Supancho <p.supancho@gmail.com>
2020-03-25 00:18:13 +0700

    add css policy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom'
-import Route from 'react-router-dom/Route'
+import { HashRouter, Route, Link } from "react-router-dom";
+// import Route from 'react-router-dom/Route'
 // import logo from './logo.svg';
 import duck from './assets/img/duck.svg'
 import profile from './assets/img/profile.jpg'
@@ -15,11 +15,24 @@ import Policy from './components/Policy'
 
 function App() {
   return (
-    <Router>
-      <Route path="/" exact component={Main}/>
-      <Route path="/duck-follow" exact component={Main}/>
-      <Route path="/duck-follow/policy-privacy" exact component={Policy}/>
-    </Router>
+    // <Router>
+    //   <Route path="/" exact component={Main}/>
+    //   <Route path="/duck-follow" exact component={Main}/>
+    //   <Route path="/duck-follow/policy-privacy" exact component={Policy}/>
+    // </Router>
+    <HashRouter basename="/">
+    <div>
+      {/* <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+
+      <hr /> */}
+
+      <Route exact path="/" component={Main} />
+      <Route path="/policy-privacy" component={Policy} />
+    </div>
+  </HashRouter>
   );
 }
 
--- src/components/Policy.js
+++ src/components/Policy.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../App.css';
+import './Policy.css';
 import { Badge } from 'reactstrap';
 
 export default class Policy extends React.Component {
@@ -8,6 +8,7 @@ export default class Policy extends React.Component {
             <div className="App">
                 <div className="App-content">
                     <h1>Policy <Badge color="secondary">privacy</Badge></h1>
+                    <h3>เมื่อคุณใช้บริการของเรา คุณไว้วางใจให้เรารักษาข้อมูลของคุณ <br/> เราเข้าใจว่านี่คือความรับผิดชอบที่ยิ่งใหญ่<br/>และพยายามอย่างยิ่งที่จะปกป้องข้อมูลของคุณ <br/> รวมถึงมอบอำนาจในการควบคุมข้อมูลให้แก่คุณ</h3>
                 </div>
             </div>
         );
