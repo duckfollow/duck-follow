7c6942a6bc0fb035d44209affb4f451adab8be46
Prasit Supancho <p.supancho@gmail.com>
2020-07-20 23:26:37 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/duck-follow/precache-manifest.9f6035faa23d4f79ecda23f72f6b1e82.js"
+  "/duck-follow/precache-manifest.da9d7924bba7cf49a555d882aa8d9065.js"
 );
 
 self.addEventListener('message', (event) => {
